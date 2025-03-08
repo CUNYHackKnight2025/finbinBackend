9bcfc4b09973a3d4df045c69282b6b741f5c57bc
chitangchin <chitangb.chin@gmail.com>
2025-03-08 09:54:55 -0500

    Spinning up Azure sql

--- Controllers/BudgetController.cs
+++ Controllers/BudgetController.cs
@@ -1,6 +1,36 @@
 /*
 Going to expose tthe endpoints here for adding and getting some budget data~
-GET
-POST
-
+GET all buckets
+POST a bucket
 */
+
+using Microsoft.AspNetCore.Mvc;
+using BudgetBackend.Data;
+using BudgetBackend.Models;
+
+[Route("api/budget")]
+[ApiController]
+public class BudgetController : ControllerBase
+{
+    private readonly ApplicationDbContext _context;
+
+    public BudgetController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        var records = _context.BudgetRecords.ToList();
+        return Ok(records);
+    }
+
+    [HttpPost]
+    public IActionResult AddRecord([FromBody] BudgetRecord record)
+    {
+        _context.BudgetRecords.Add(record);
+        _context.SaveChanges();
+        return Ok(record);
+    }
+}
