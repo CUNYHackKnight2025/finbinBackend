f2634881630db96fff592814d1bc8028a5bfcd32
chitangchin <chitangb.chin@gmail.com>
2025-03-08 09:30:28 -0500

    Added the DB setup from MS ET Core

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -2,3 +2,15 @@
 Were going to use EF core to query and manipulate data without writing any raw SQL queries
 So we want to add the bridge between our objects and our db
 */
+
+using Microsoft.EntityFrameworkCore;
+using BudgetBackend.Models;
+
+namespace BudgetBackend.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    public DbSet<BudgetRecord> BudgetRecords { get; set; }
+}
